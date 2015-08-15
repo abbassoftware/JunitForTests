61277501f1e71677d97d49a8c21ce0b3025f3cf4
Abbas Suterwala <abbas.s786@yahoo.co.in>
2015-08-15 16:27:55 +0530

    Some formatting modification

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,34 +1,47 @@
 
 public class MyMath {
-	
-	public int add(int a, int b) {
-		if(a < 0 || b < 0) {
-			throw new UnsupportedOperationException();
-		}
-		
-		return a + b;
-	}
-	
-	public int findMax(int a[]) {
-		int max = -1;
-		for(int i =0 ; i < a.length ; i++) {
-			if(a[i] > max) {
-				max = a[i];
-			}
-		}
-		return max;		
-	}
-	
-	public void sort(int a[]) {
-		for(int i = 0 ; i < a.length ; i++) {
-			for(int j= 0 ; j < a.length ; j++) {
-				if(a[j] > a[j+1]) {
-					int temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
-				}
-			}
-		}
-	}
+
+    /**
+     * @param a
+     * @param b
+     * @return - The addition od a and b.
+     */
+    public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new UnsupportedOperationException();
+        }
+        return a + b;
+    }
+
+    /**
+     * @param a
+     * @return - The maximum element in the array
+     */
+    public int findMax(int a[]) {
+        int max = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    /**
+     * This function sorts an array in ascending order.
+     * 
+     * @param a
+     */
+    public void sort(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
 
 }
--- src/MyMathTests.java
+++ src/MyMathTests.java
@@ -5,62 +5,60 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MyMathTests {
-	
-	int a[] = null;
-	
-	@Before
-	public void setup() {
-		a = new int[]{11, 15 , 2, 3};
-	}
 
-	@Test
-	public void test_add_valid() {
-		MyMath myMath = new MyMath();
-		int result = myMath.add(5, 6);
-		assertEquals(result, 11);
-	}
-	
-	@Test
-	public void test_max_valid() {
-		//int a [] = new int[]{11, 15 , 2, 3};
-		MyMath myMath = new MyMath();
-		int max = myMath.findMax(a);
-		assertEquals(max, 15);
-		
-	}
-	
-	@Test
-	public void test_sort_valid() {
-		//int a [] = new int[]{11, 15 , 2, 3};
-		int sortedReference [] = new int[]{2, 3, 11, 15};
-		MyMath myMath = new MyMath();
-		myMath.sort(a);
-		assertArrayEquals(a, sortedReference);
-	}
-	
-	
-	@Test 
-	public void test_add_exception1() {
-		try {
-			MyMath myMath = new MyMath();
-			int result = myMath.add(-5, 6);
-			fail("Should have got an UnsupportedOperationException");
-		} catch (UnsupportedOperationException e) {
-			//Pass - UnsupportedOperationException is expected.
-		}
-		
-	} 
-	
-	@Test (expected = UnsupportedOperationException.class)
-	public void test_add_exception2() {
-		MyMath myMath = new MyMath();
-		int result = myMath.add(-5, 6);
-	}
-	
-	@After
-	public void cleanup() {
-		a = null;
-	}
-	
+    int a[] = null;
+
+    @Before
+    public void setup() {
+        a = new int[] { 11, 15, 2, 3 };
+    }
+
+    @Test
+    public void test_add_valid() {
+        MyMath myMath = new MyMath();
+        int result = myMath.add(5, 6);
+        assertEquals(result, 11);
+    }
+
+    @Test
+    public void test_max_valid() {
+        // int a [] = new int[]{11, 15 , 2, 3};
+        MyMath myMath = new MyMath();
+        int max = myMath.findMax(a);
+        assertEquals(max, 15);
+
+    }
+
+    @Test
+    public void test_sort_valid() {
+        // int a [] = new int[]{11, 15 , 2, 3};
+        int sortedReference[] = new int[] { 2, 3, 11, 15 };
+        MyMath myMath = new MyMath();
+        myMath.sort(a);
+        assertArrayEquals(a, sortedReference);
+    }
+
+    @Test
+    public void test_add_exception1() {
+        try {
+            MyMath myMath = new MyMath();
+            int result = myMath.add(-5, 6);
+            fail("Should have got an UnsupportedOperationException");
+        } catch (UnsupportedOperationException e) {
+            // Pass - UnsupportedOperationException is expected.
+        }
+
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void test_add_exception2() {
+        MyMath myMath = new MyMath();
+        int result = myMath.add(-5, 6);
+    }
+
+    @After
+    public void cleanup() {
+        a = null;
+    }
 
 }
